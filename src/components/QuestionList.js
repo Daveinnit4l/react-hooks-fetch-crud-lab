71da15198cb98e201e828f36ef690180d9802f89
kyle <davek2647@gmail.com>
2023-11-16 05:15:35 +0300

    done

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
-  return (
-    <section>
-      <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
-  );
+class QuestionList extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      questions: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => this.setState({ questions: data }));
+  }
+
+  handleUpdateQuestion = (id, correctIndex) => {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          const { questions } = this.state;
+          const updatedQuestions = questions.map((question) => {
+            if (question.id === id) {
+              return { ...question, correctIndex };
+            }
+            return question;
+          });
+          this.setState({ questions: updatedQuestions });
+        }
+      });
+  }
+
+  render() {
+    const { questions } = this.state;
+
+    return (
+      <section>
+        <h1>Quiz Questions</h1>
+        <ul>
+          {questions.map((question) => (
+            <QuestionItem
+              key={question.id}
+              question={question}
+              onUpdate={this.handleUpdateQuestion}
+            />
+          ))}
+        </ul>
+      </section>
+    );
+  }
 }
 
 export default QuestionList;
+
